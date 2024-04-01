6b5997459acb9a95b683a922544b4ea281f07848
Oleh Melenevskyi <oleh.melenevskyi@ext.runpanther.io>
2024-04-01 12:02:59 +0300

    Add panther_mongodb_helpers

---
+++ global_helpers/panther_mongodb_helpers.py
@@ -0,0 +1,9 @@
+from panther_base_helpers import deep_get
+
+
+def mongodb_alert_context(event) -> dict:
+    return {
+        "username": event.get("username", "<USER_NOT_FOUND>"),
+        "target_username": event.get("targetUsername", "<USER_NOT_FOUND>"),
+        "org_id": event.get("orgId", "<ORG_NOT_FOUND>"),
+    }
